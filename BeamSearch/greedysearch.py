bf2a96ec9c209e7a996593a19567cea87de8cba7
nawnoes <nawnoes@gmail.com>
2020-03-27 16:14:05 +0900

    Beam Search & Greedy Search

---
+++ BeamSearch/greedysearch.py
@@ -0,0 +1,27 @@
+"""
+source by https://machinelearningmastery.com/beam-search-decoder-natural-language-processing/
+"""
+
+from numpy import array
+from numpy import argmax
+
+# greedy decoder
+def greedy_decoder(data):
+	# index for largest probability each row
+	return [argmax(s) for s in data]
+
+# define a sequence of 10 words over a vocab of 5 words
+data = [[0.1, 0.2, 0.3, 0.4, 0.5],
+		[0.5, 0.4, 0.3, 0.2, 0.1],
+		[0.1, 0.2, 0.3, 0.4, 0.5],
+		[0.5, 0.4, 0.3, 0.2, 0.1],
+		[0.1, 0.2, 0.3, 0.4, 0.5],
+		[0.5, 0.4, 0.3, 0.2, 0.1],
+		[0.1, 0.2, 0.3, 0.4, 0.5],
+		[0.5, 0.4, 0.3, 0.2, 0.1],
+		[0.1, 0.2, 0.3, 0.4, 0.5],
+		[0.5, 0.4, 0.3, 0.2, 0.1]]
+data = array(data)
+# decode sequence
+result = greedy_decoder(data)
+print(result)
