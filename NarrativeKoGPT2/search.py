7c6eb9b9a55dc3f76ccb72189dff2801ab922424
nawnoes <nawnoes@gmail.com>
2020-04-08 18:03:21 +0900

    random and beam search

--- NarrativeKoGPT2/search.py
+++ NarrativeKoGPT2/search.py
@@ -8,14 +8,14 @@ def randomSearch(predict, k, vocab):
   # topk 중 랜덤으로 선택된 값을 반환.
 
   prob, indexs = torch.topk(predict, k=k, dim=-1)
-  # indexs = indexs.data.view(-1)
-  print('topk indexs: ', indexs[-1])
+  indexs = indexs.squeeze().tolist()[-1]
+  # print('topk indexs: ', indexs)
 
-  # gen = [vocab.to_tokens(index.squeeze().tolist())[-1] for index in indexs]
+  gen = [vocab.to_tokens(index) for index in indexs]
 
   # print('topk value: ', prob)
-  # print('topk count: ', index)
-  # print('topk word: ', gen)
+  # print('topk count: ', indexs)
+  print('topk word: ', gen)
   del indexs
 
 
